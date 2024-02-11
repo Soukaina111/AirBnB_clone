41552e9f564accd73eeb1af88f1e09c8b34f2044
Soukaina111 <basmahajar@gmail.com>
2024-02-11 12:24:02 -0800

    BM2

--- models/base_model.py
+++ models/base_model.py
@@ -22,6 +22,7 @@ class BaseModel:
             self.id = str(uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            models.storage.new(self)
             return
 
         if 'id' not in kwargs:
@@ -46,6 +47,7 @@ class BaseModel:
     def save(self):
         """stores the updated attribute"""
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """displays the dictionary representation of self"""
@@ -54,3 +56,15 @@ class BaseModel:
         dict['created_at'] = self.created_at.strftime('%Y-%m-%dT%H:%M:%S.%f')
         dict['updated_at'] = self.updated_at.strftime('%Y-%m-%dT%H:%M:%S.%f')
         return dict
+
+    def update(cls, att_id, *args):
+            """Updates an instance considering multiple cases"""
+            if len(args) == 0:
+                  print("** attribute name missing **")
+                  return
+            if len(args) == 1 and isinstance(args[0], dict):
+                  for key, value in args[0].items():
+                        models.storage.single_modif(cls.__name__, att_id, key, value)
+            else:
+                  if len(args) >= 2:
+                        models.storage.single_modif(cls.__name__, att_id, args[0], args[1])
