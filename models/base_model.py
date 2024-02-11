842ad8fa7a726321f54305a0e896e71634156477
Soukaina111 <basmahajar@gmail.com>
2024-02-11 14:36:34 -0800

    STYLE

--- models/base_model.py
+++ models/base_model.py
@@ -3,9 +3,11 @@ from models import storage
 import uuid
 from datetime import datetime
 
+
 class BaseModel:
     """
-    A base class that defines common attributes and methods for other classes.
+    A base class that defines commo
+    attributes and methods for other classes.
     """
 
     def __init__(self, *args, **kwargs):
@@ -14,22 +16,24 @@ class BaseModel:
 
         Args:
             *args: Not used.
-            **kwargs: A dictionary containing attribute names and their corresponding values.
-
+            **kwargs: A dictionary containing attribute
+            names and their corresponding values.
         If kwargs is not empty:
             - Each key of this dictionary is an attribute name.
-            - Each value of this dictionary is the value of the attribute name.
-            - The 'created_at' and 'updated_at' attributes are converted from strings to datetime objects.
-
+            - Each value of this dictionary is the value of the
+            attribute name.
+            - The 'created_at' and 'updated_at' attributes are
+             converted from strings to datetime objects.
         If kwargs is empty:
-            - The id and created_at attributes are created as in the previous implementation.
+            - The id and created_at attributes are
+            created as in the previous implementation.
         """
         if kwargs:
-            for key, value in kwargs.items():
+            for key, val in kwargs.items():
                 if key != '__class__':
                     if key in ['created_at', 'updated_at']:
-                        value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
-                    setattr(self, key, value)
+                        val = datetime.strptime(val, '%Y-%m-%dT%H:%M:%S.%f')
+                    setattr(self, key, val)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
@@ -41,9 +45,11 @@ class BaseModel:
         Returns a string representation of the object.
 
         Returns:
-            str: A string representation in the format [<class name>] (<id>) <__dict__>.
+            str: A string representation in
+            the format [<class name>] (<id>) <__dict__>.
         """
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+        cls = self.__class__.name__
+        return "[{}] ({}) {}".format(cls, self.id, self.__dict__)
 
     def save(self):
         """
@@ -57,9 +63,12 @@ class BaseModel:
         Returns a dictionary representation of the object.
 
         Returns:
-            dict: A dictionary containing all keys/values of __dict__ of the instance.
-                  The dictionary includes the __class__ key with the class name and converts the created_at
-                  and updated_at attributes to ISO format (%Y-%m-%dT%H:%M:%S.%f).
+            dict: A dictionary containing all keys/values of
+            __dict__ of the instance.
+                  The dictionary includes the __class__ key
+                  with the class name and converts the created_at
+                  and updated_at attributes to
+                  ISO format (%Y-%m-%dT%H:%M:%S.%f).
         """
         obj_dict = self.__dict__.copy()
         obj_dict['__class__'] = self.__class__.__name__
