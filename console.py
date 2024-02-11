3e8535f0e1897bbc23bdb961012e13a762802840
SaidLamghari <said.lmg@gmail.com>
2024-02-11 12:43:23 -0800

    console.py

--- console.py
+++ console.py
@@ -1,125 +1,137 @@
 #!/usr/bin/python3
-""" Starts the entry point of the Airbnb Project-Console
-"""
-
-from models import storage
-from cmd import Cmd
-from models.engine.errors import *
-from models.amenity import Amenity
+import cmd
 from models.base_model import BaseModel
-from models.place import Place
-from models.user import User
 from models.state import State
 from models.city import City
+from models.amenity import Amenity
+from models.place import Place
 from models.review import Review
+from models import storage
 
-Entities = storage.models
+class HBNBCommand(cmd.Cmd):
+    """
+    This class represents a command-line interface for managing objects.
 
+    It provides various commands for creating, retrieving, updating, and deleting objects.
+    """
 
-class HBNBCommand(Cmd):
-    """ have multiple commands that we overrided """
     prompt = "(hbnb) "
 
-    def do_EOF(self, args):
-        """End of file function ovverided"""
-        print()
-        return True
+    def do_show(self, arg):
+        """
+        Retrieve and display the details of a specific instance.
 
-    def do_quit(self, args):
-        """a function that quits the program"""
-        print()
-        return True
-
-    def emptyline(self):
-        """executed when we press enter and do nothing """
-        pass
-
-    def do_create(self, line):
-        """Creates a new instance of a model name"""
-
-        line = line.split()
-
-        if len(line) == 0:
+        Usage: show <class name> <object id>
+        """
+        args = arg.split()
+        if len(args) < 2:
             print("** class name missing **")
-        elif line[0] not in Entities:
+            return
+        class_name = args[0]
+        obj_id = args[1]
+        objects = storage.all()
+        key = "{}.{}".format(class_name, obj_id)
+        if key in objects:
+            print(objects[key])
+        else:
+            print("** no instance found **")
+
+    def do_create(self, arg):
+        """
+        Create a new instance of a class and save it.
+
+        Usage: create <class name>
+        """
+        if not arg:
+            print("** class name missing **")
+            return
+        class_name = arg
+        if class_name not in storage.classes:
             print("** class doesn't exist **")
-        elif len(line) == 1:
-            searched = eval(line[0])()
-            print(searched.id)
-            searched.save()
-        else:
-            print("** Can't create because their is too many arguments **")
+            return
+        obj = storage.classes[class_name]()
+        obj.save()
+        print(obj.id)
 
-    def do_show(self, line):
-        """Prints the string representation of an
-        instance based on the class name and id"""
+    def do_destroy(self, arg):
+        """
+        Delete a specific instance.
 
-        input = line.split()
-        if not input:
+        Usage: destroy <class name> <object id>
+        """
+        args = arg.split()
+        if len(args) < 2:
             print("** class name missing **")
-        elif len(input) == 1:
-            print("** instance id missing **")
-        elif len(input) == 2:
-            try:
-                obj = storage.search_id(*input)
-                print(obj)
-            except ModelNotFoundError:
-                print("** class doesn't exist **")
-            except InstanceNotFoundError:
-                print("** no instance found **")
+            return
+        class_name = args[0]
+        obj_id = args[1]
+        objects = storage.all()
+        key = "{}.{}".format(class_name, obj_id)
+        if key in objects:
+            del objects[key]
+            storage.save()
         else:
-            print("** Too many arguments for show **")
+            print("** no instance found **")
 
-    def do_destroy(self, line):
-        """Removes an instance of a given entity using id and class model"""
-        output = line.split()
+    def do_update(self, arg):
+        """
+        Update the attributes of a specific instance.
 
-        if not output:
+        Usage: update <class name> <object id> <attribute name> <value>
+        """
+        args = arg.split()
+        if len(args) < 2:
             print("** class name missing **")
-        elif len(output) == 1:
-            print("** instance id missing **")
-        elif len(output) == 2:
-            try:
-                storage.destroy_id(*output)
-            except ModelNotFoundError:
-                print("** class doesn't exist **")
-            except InstanceNotFoundError:
-                print("** no instance found **")
-        else:
-            print("** There is too  many arguments To destroy **")
-
-    def do_all(self, line):
-        """Display string representations of
-        all instances of a given class."""
-
-        line = line.split()
-        if len(line) < 2:
-            try:
-                print(storage.retrieve_data(*line))
-            except ModelNotFoundError:
-                print("** class doesn't exist **")
-            else:
-                print("** Too many arguments for all **")
-
-    def do_update(self, line):
-        """Updates an instance based on its id"""
-        input = line.split()
-        if len(input) == 0:
-            print("** class name missing **")
-        elif len(input) == 1:
-            print("** instance id missing **")
-        elif len(input) == 2:
+            return
+        class_name = args[0]
+        obj_id = args[1]
+        objects = storage.all()
+        key = "{}.{}".format(class_name, obj_id)
+        if key not in objects:
+            print("** no instance found **")
+            return
+        if len(args) < 3:
             print("** attribute name missing **")
-        elif len(input) == 3:
+            return
+        if len(args) < 4:
             print("** value missing **")
-        else:
-            try:
-                storage.single_modif(*input[0:4])
-            except ModelNotFoundError:
-                print("** class doesn't exist **")
-            except InstanceNotFoundError:
-                print("** no instance found **")
+            return
+        attribute_name = args[2]
+        value = args[3]
+        obj = objects[key]
+        setattr(obj, attribute_name, value)
+        obj.save()
 
+    def do_all(self, arg):
+        """
+        Display all instances or instances of a specific class.
 
-if _name_ == "_main_":
+        Usage: all [class name]
+        """
+        objects = storage.all()
+        if not arg:
+            print([str(obj) for obj in objects.values()])
+            return
+        class_name = arg
+        if class_name not in storage.classes:
+            print("** class doesn't exist **")
+            return
+        filtered_objects = [str(obj) for obj in objects.values() if isinstance(obj, storage.classes[class_name])]
+        print(filtered_objects)
+
+    def do_quit(self, arg):
+        """
+        Quit the command-line interface.
+        """
+        print()
+        return True
+
+    def do_EOF(self, arg):
+        """
+        Handle the end-of-file signal to exit the command-line interface.
+        """
+        print()
+        return True
+
+if __name__ == '__main__':
     HBNBCommand().cmdloop()
