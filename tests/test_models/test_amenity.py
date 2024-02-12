997946c9bc944d416649d0deb48e795ff29b0587
Soukaina111 <basmahajar@gmail.com>
2024-02-12 12:25:51 -0800

    MORE testsS2024

---
+++ tests/test_models/test_amenity.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+""" this is a unittest for subclass Amenity """
+
+import unittest
+from models.amenity import Amenity
+
+
+class TestAmenityModel(unittest.TestCase):
+    """ this is a unittest for subclass Amenity """
+
+def test_amenity(self):
+        amenity = Amenity()
+        amenity.name = "Payed Subscription"
+        self.assertEqual(amenity.name, "Payed Subscription")
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ tests/test_models/test_city.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+""" this is a unittest for subclass City """
+
+import unittest
+from uuid import uuid4
+from models.city import City
+
+class TestCityModel(unittest.TestCase):
+    """ this is a unittest for subclass City """
+
+def test_city(self):
+        """ this is a unittest for subclass City """
+        state_id = uuid4()
+        city = City()
+        city.name = "Casablanca"
+        city.state_id = state_id
+        self.assertEqual(city.name, "Casablanca")
+        self.assertEqual(city.state_id, state_id)
+
+if __name__ == "__main__":
+    unittest.main()
