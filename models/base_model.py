17d3772c191e2e31aee0463f29c3bad54f5c1b6e
Soukaina111 <basmahajar@gmail.com>
2024-02-12 08:44:12 -0800

    FIIILE2003

--- models/base_model.py
+++ models/base_model.py
@@ -49,7 +49,6 @@ class BaseModel:
             str: A string representation in
             the format [<class name>] (<id>) <__dict__>.
         """
-        cls = self.__class__.__name__
         return "[{}] ({}) {}".format(cls, self.id, self.__dict__)
 
     def save(self):
