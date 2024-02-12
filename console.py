3b2e7aabc70e8570fbe3ee9d1f4f5ab7ab8aa43c
Soukaina111 <basmahajar@gmail.com>
2024-02-12 11:34:27 -0800

    Styyyle

--- console.py
+++ console.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """ Starts the entry point of the Airbnb Project-Console
 """
-
+from models import storage
 from cmd import Cmd
+
+Entities = storage.models
 
 
 class HBNBCommand(Cmd):
@@ -22,6 +24,88 @@ class HBNBCommand(Cmd):
     def emptyline(self):
         """executed when we press enter and do nothing """
         pass
+
+    def do_create(self, line):
+        """Creates a new instance of a given model name"""
+        line = line.split()
+        if len(line) == 0:
+            print("** class name missing **")
+        elif line[0] not in Entities:
+            print("** class doesn't exist **")
+        elif len(line) == 1:
+            searched = eval(line[0])()
+            print(searched.id)
+            searched.save()
+        else:
+            print("** Can't create because their is too many arguments **")
+
+    def do_show(self, line):
+        """Prints the string representation
+        of an instance based on the class name and id"""
+        input = line.split()
+        if not input:
+            print("** class name missing **")
+        elif len(input) == 1:
+            print("** instance id missing **")
+        elif len(input) == 2:
+            try:
+                obj = storage.search_id(*input)
+                print(obj)
+            except ModelNotFoundError:
+                print("** class doesn't exist **")
+            except InstanceNotFoundError:
+                print("** no instance found **")
+        else:
+            print("** Too many arguments for show **")
+
+    def do_destroy(self, line):
+        """Removes an instance of a given entity using id and class model"""
+        output = line.split()
+
+        if not output:
+            print("** class name missing **")
+        elif len(output) == 1:
+            print("** instance id missing **")
+        elif len(output) == 2:
+            try:
+                storage.destroy_id(*output)
+            except ModelNotFoundError:
+                print("** class doesn't exist **")
+            except InstanceNotFoundError:
+                print("** no instance found **")
+        else:
+            print("** There is too  many arguments To destroy **")
+
+    def do_all(self, line):
+        """Display string representations
+        of all instances of a given class."""
+        line = line.split()
+        if len(line) < 2:
+            try:
+                print(storage.retrieve_data(*line))
+            except ModelNotFoundError:
+                print("** class doesn't exist **")
+        else:
+            print("** Too many arguments for all **")
+
+    def do_update(self, line):
+        """Updates an instance based on its id"""
+        input = line.split()
+        if len(input) == 0:
+            print("** class name missing **")
+        elif len(input) == 1:
+            print("** instance id missing **")
+        elif len(input) == 2:
+            print("** attribute name missing **")
+        elif len(input) == 3:
+            print("** value missing **")
+        else:
+            try:
+                storage.single_modif(*input[0:4])
+            except ModelNotFoundError:
+                print("** class doesn't exist **")
+            except InstanceNotFoundError:
+                print("** no instance found **")
 
 
 if __name__ == '__main__':
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -17,6 +17,11 @@ class FileStorage:
 
     __objects: dict = {}
     __file_path: str = 'file.json'
+    models = (
+            "BaseModel",
+            "User", "City", "State", "Place",
+            "Amenity", "Review"
+            )
 
     def __init__(self):
         """Initializer"""
@@ -49,3 +54,57 @@ class FileStorage:
                     for k, obj in to_deser.items()}
         except (FileNotFoundError, JSONDecodeError):
             pass
+
+    def search_id(self, mdl, object_id):
+        """search an instance using the id  we passed as an argument"""
+        FS = FileStorage
+        if mdl not in FS.models:
+            raise ModelNotFoundError(mdl)
+        key = mdl + "." + object_id
+        if key not in FS.__objects:
+            raise InstanceNotFoundError(object_id, mdl)
+        return FS.__objects[key]
+
+    def destroy_id(self, entity, obj_id):
+        """destroys an instance using the id  we pass as an argument"""
+        FS = FileStorage
+        if entity not in FS.models:
+            raise ModelNotFoundError(entity)
+
+        k = entity + "." + obj_id
+        if k not in FS.__objects:
+            raise InstanceNotFoundError(obj_id, entity)
+
+        del FS.__objects[k]
+        self.save()
+
+    def retrieve_data(self, entity=""):
+        """Retrieves all object of an entity"""
+        if entity and entity not in FileStorage.models:
+            raise ModelNotFoundError(entity)
+        outcome = []
+        for key, val in FileStorage.__objects.items():
+            if key.startswith(entity):
+                outcome.append(str(val))
+        return outcome
+
+    def single_modif(self, entity, UI, domain, val):
+        """Updates an instance"""
+        FS = FileStorage
+        if entity not in FS.models:
+            raise ModelNotFoundError(entity)
+
+        key = entity + "." + UI
+        if key not in FS.__objects:
+            raise InstanceNotFoundError(UI, entity)
+        if domain in ("id", "updated_at", "created_at"):
+            return
+        obj = FS.__objects[key]
+        try:
+            o1 = type(obj.__dict__[domain])
+            obj.__dict__[domain] = o1(val)
+        except KeyError:
+            obj.__dict__[domain] = val
+        finally:
+            obj.updated_at = datetime.utcnow()
+            self.save()
