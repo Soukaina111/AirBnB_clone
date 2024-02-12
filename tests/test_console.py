6e1071cc4a577f6946edf0d1ed3e6fc3c3f4a436
SaidLamghari <said.lmg@gmail.com>
2024-02-12 15:52:03 -0800

    test_console.py

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,40 +1,42 @@
 #!/usr/bin/python3
 import unittest
 from console import HBNBCommand
+import sys
 
 
 class TestHBNBCommand(unittest.TestCase):
     def setUp(self):
         self.console = HBNBCommand()
+        self.stdout = sys.stdout
+        sys.stdout = self._string_io = StringIO()
 
     def tearDown(self):
-        pass
+        sys.stdout = self.stdout
 
     def test_create_missing_class_name(self):
-        with patch('sys.stdout', new=StringIO()) as output:
-            self.console.onecmd('create')
-            self.assertEqual(output.getvalue().strip(), "** class name missing **")
+        self.console.onecmd('create')
+        output = self._string_io.getvalue().strip()
+        self.assertEqual(output, "** class name missing **")
 
     def test_create_invalid_class_name(self):
-        with patch('sys.stdout', new=StringIO()) as output:
-            self.console.onecmd('create InvalidClass')
-            self.assertEqual(output.getvalue().strip(), "** class doesn't exist **")
+        self.console.onecmd('create InvalidClass')
+        output = self._string_io.getvalue().strip()
+        self.assertEqual(output, "** class doesn't exist **")
 
     def test_create_valid_class_name(self):
-        with patch('sys.stdout', new=StringIO()) as output:
-            self.console.onecmd('create BaseModel')
-            result = output.getvalue().strip()
-            self.assertTrue(result)
+        self.console.onecmd('create BaseModel')
+        result = self._string_io.getvalue().strip()
+        self.assertTrue(result)
 
     def test_show_missing_class_name(self):
-        with patch('sys.stdout', new=StringIO()) as output:
-            self.console.onecmd('show')
-            self.assertEqual(output.getvalue().strip(), "** class name missing **")
+        self.console.onecmd('show')
+        output = self._string_io.getvalue().strip()
+        self.assertEqual(output, "** class name missing **")
 
     def test_show_missing_instance_id(self):
-        with patch('sys.stdout', new=StringIO()) as output:
-            self.console.onecmd('show BaseModel')
-            self.assertEqual(output.getvalue().strip(), "** instance id missing **")
+        self.console.onecmd('show BaseModel')
+        output = self._string_io.getvalue().strip()
+        self.assertEqual(output, "** instance id missing **")
 
 
 if __name__ == '__main__':
