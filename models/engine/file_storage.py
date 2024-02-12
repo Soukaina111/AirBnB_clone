f9555f5728052c3861ef6be792dabf0747ba371c
Soukaina111 <basmahajar@gmail.com>
2024-02-12 12:16:24 -0800

    MORE CLASSES123

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -10,6 +10,12 @@ from json.decoder import JSONDecodeError
 from datetime import datetime
 from models.engine.errors import *
 from models.user import User
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
 
 class FileStorage:
